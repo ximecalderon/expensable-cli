4c7ff23f983b16de0c2bff6673201c44d4a03a97
Ximena Calderón <mxcalderons@gmail.com>
2021-12-09 18:49:20 -0500

    Tested show, toggle, next and prev methods for categories and transactions

--- handlers/categories_handler.rb
+++ handlers/categories_handler.rb
@@ -1,31 +1,56 @@
 module CategoriesHandler
-  def categories_table(transaction_type)
+  # ---------- methods for table printing ---------- #
+  def categories_table
+    case @expense
+    when true
+      transaction_type = "expense"
+      title = "Expenses"
+    when false
+      transaction_type = "income"
+      title = "Income"
+    end
+
     categories_data = @categories.select do |category|
       category[:transaction_type] == transaction_type
     end
 
     table = Terminal::Table.new
-    table.title = transaction_type.capitalize
+    table.title = "#{title}\n#{@date.strftime('%B %Y')}"
     table.headings = ["ID", "Category", "Total"]
     table.rows = categories_data.map do |category|
-      [category[:id], category[:name], transactions_amount_sum(category)]
+      [category[:id], category[:name], transactions_amount_sum(category[:transactions])]
     end
     table
   end
 
-  def expenses_table
-    categories_table("expense")
-  end
+  def transactions_amount_sum(transactions)
+    month_data = month_transactions(transactions)
 
-  def income_table
-    categories_table("income")
-  end
-
-  def transactions_amount_sum(category)
-    amounts = category[:transactions].map do |transaction|
+    amounts = month_data.map do |transaction|
       transaction[:amount]
     end
-
     amounts.sum
+  end
+
+  def toggle
+    @expense = @expense ? false : true
+    categories_table
+  end
+
+  # ---------- Date handling methods ---------- #
+  def next_month_categories
+    @date = @date.next_month
+    categories_table
+  end
+
+  def prev_month_categories
+    @date = @date.prev_month
+    categories_table
+  end
+
+  def month_transactions(transactions)
+    transactions.select do |transaction|
+      Date.parse(transaction[:date]).month == @date.month
+    end
   end
 end
--- services/transaction.rb
+++
@@ -1,6 +0,0 @@
-require "httparty"
-require "json"
-
-class Transaction
-  include HTTParty
-end
