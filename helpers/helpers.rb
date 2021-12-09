b14bba6f5a41e8846bfcd2a5807a9cf5ec85857f
Ximena Calderón <mxcalderons@gmail.com>
2021-12-09 11:58:18 -0500

    correction to amount value in expenses table

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -15,7 +15,6 @@ module Helpers
     ].join("\n")
   end
 
-
   def get_string(label, required: false)
     input = ""
     loop do
@@ -30,12 +29,13 @@ module Helpers
 
   def credentials_form
     email = get_string("Email", required: true)
-    password = get_string("Password",required: true)
+    password = get_string("Password", required: true)
 
-    { email: email, password: password } 
+    { email: email, password: password }
   end
 
-  def get_with_options(options1, options2 = nil, required: true, default: nil) # (options_line1, options_line2 = nil)
+  # (options_line1, options_line2 = nil)
+  def get_with_options(options1, options2 = nil, required: true, default: nil)
     action = ""
     id = nil
     loop do
@@ -44,6 +44,7 @@ module Helpers
       print "> "
       action, id = gets.chomp.split
       break if options1.include?(action) || options2.include?(action) || (action.nil? && !required)
+
       puts "Invalid option"
     end
     action.nil? && default ? [default] : [action, id]
@@ -55,15 +56,15 @@ module Helpers
 
   def categories_menu
     get_with_options(
-        ["create", "show ID", "update ID", "delete ID"],
-        ["add-to ID", "toggle", "next", "prev", "logout"]
+      ["create", "show ID", "update ID", "delete ID"],
+      ["add-to ID", "toggle", "next", "prev", "logout"]
     )
   end
 
   def transactions_menu
     get_with_options(
-        ["add", "update ID", "delete ID"],
-        ["next", "prev", "back"]
+      ["add", "update ID", "delete ID"],
+      ["next", "prev", "back"]
     )
   end
 end
